8e161c26b20d2fec5cccd64e18fe3ceab74ba28b
ViniciuSvD <107283882+ViniciuSvD@users.noreply.github.com>
2023-11-17 22:42:21 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Sarah, eu amo você! Se você também me ama, dá uma risadinha. :D</h1>
+  return (
+    <h1>Sarah, eu amo você! Se você também me ama, dá uma risadinha. :D</h1>
+  );
 }
 
 export default Home;
